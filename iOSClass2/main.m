eeffcb0ffea7eed78ffd733da17690877744b860
Sean Riggle <seanriggle@Seans-MacBook-Air.local>
2013-03-06 19:27:06 -0500

    Initial Commit

---
+++ iOSClass2/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  iOSClass2
+//
+//  Created by Sean Riggle on 3/6/13.
+//  Copyright (c) 2013 Sean Riggle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
